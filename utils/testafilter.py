ee496d3cfeeddb16d62e8abbc20f01940a319fbd
Carlos H Mora Jr <59943553+mora72@users.noreply.github.com>
2020-02-02 14:36:22 -0300

    Delete testafilter.py

--- utils/testafilter.py
+++
@@ -1,7 +0,0 @@
-from lib.arquivos.bacarquivos import *
-
-listacontas = lerarqcontas()
-print(listacontas)
-print(list(filter(lambda conta: conta["nome"] == "Aluguel Guilherme", listacontas)))
-listares = list(filter(lambda conta: (conta["nome"] == "Aluguel Guilherme" and conta["tipo"] == "R"), listacontas))
-print(listares[0]["tipo"])
